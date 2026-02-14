de97167c02805e35f93b45598030fbdd0148fafd
Shaurya <shaurya@example.com>
2026-02-14 17:12:26 +0530

    added depth PID object to docking controller

--- src/mira2_path_planning/src/docking_controller.cpp
+++ src/mira2_path_planning/src/docking_controller.cpp
@@ -9,6 +9,7 @@ class DockingController : public rclcpp::Node {
     public:
     DockingController() : Node("docking_controller"), forwardPIDObj(), lateralPIDObj() {
         // Initialize PID gains
+        depthPIDObj.kp = 5.5;    depthPIDObj.ki = 0.03;    depthPIDObj.kd = 31.5;  depthPIDObj.base_offset = 1500;
         forwardPIDObj.kp = -0.8;  forwardPIDObj.ki = -0.1;  forwardPIDObj.kd = -4.0;  forwardPIDObj.base_offset = 1500;
         lateralPIDObj.kp = -0.5;  lateralPIDObj.ki = -0.05; lateralPIDObj.kd = -2.0;  lateralPIDObj.base_offset = 1500;
 
@@ -29,6 +30,7 @@ class DockingController : public rclcpp::Node {
 
     private:
     // PID Controllers
+    PID_Controller depthPIDObj;
     PID_Controller forwardPIDObj;
     PID_Controller lateralPIDObj;
 
@@ -43,6 +45,7 @@ class DockingController : public rclcpp::Node {
     bool softwareArmFlag = false;
 
     // PID errors
+    double currentDepth, depthSetpoint;
     double forwardSetpoint;
     double lateralSetpoint;
     double currentHeading;
@@ -51,12 +54,14 @@ class DockingController : public rclcpp::Node {
     rclcpp::Time initTime;
 
     void telemetryCallback(const custom_msgs::msg::Telemetry::SharedPtr msg) {
-        currentHeading = msg->heading
+        currentHeading = msg->heading;
+        currentDepth = msg->external_pressure;
     }
 
     void poseCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg) {
         forwardSetpoint = msg->pose.position.x;
         lateralSetpoint = msg->pose.position.y;
+        depthSetpoint = msg->pose.position.z;
     }
 
     void mainLoop() {
@@ -70,6 +75,10 @@ class DockingController : public rclcpp::Node {
         cmdMsg.arm = true;
 
         if (softwareArmFlag) {
+            float pidDepth = depthPIDObj.pid_control(
+                depthSetpoint, (time_now - initTime).seconds(), false
+            );
+
             float pidForward = forwardPIDObj.pid_control(
                 forwardSetpoint, (timeNow - initTime).seconds(), false
             );
@@ -78,6 +87,7 @@ class DockingController : public rclcpp::Node {
                 lateralSetpoint, (timeNow - initTime).seconds(), false
             );
 
+            cmdMsg.thrust = pidDepth;
             cmdMsg.forward = pidForward;
             cmdMsg.lateral = pidLateral;
             RCLCPP_INFO(this->get_logger(), "Approaching Dock XY");
@@ -86,6 +96,7 @@ class DockingController : public rclcpp::Node {
             lateralPIDObj.emptyError();
             cmdMsg.arm = false;
             cmdMsg.mode = "ALT_HOLD";
+            cmdMsg.thrust = 1500;
             cmdMsg.forward = 1500;
             cmdMsg.lateral = 1500;
         }
